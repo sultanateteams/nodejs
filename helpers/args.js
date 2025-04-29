edc6779850cd9991f04bca5a2de77156c3910604
Sultanateteam <sultanateteam@gmail.com>
2025-04-29 21:57:25 +0500

    d

---
+++ helpers/args.js
@@ -0,0 +1,19 @@
+const getArgs = (args) => {
+  console.log(args);
+  const res = {};
+  args.slice(2).forEach((value, index, array) => {
+    if (value.charAt(0) == "-") {
+      if (index == array.length - 1) {
+        res[value.substring(1)] = true;
+      } else if (array[index + 1].charAt(0) != "-") {
+        res[value.substring(1)] = array[index + 1];
+      } else {
+        res[value.substring(1)] = true;
+      }
+    }
+  });
+  console.log(res);
+  return res;
+};
+
+module.exports = getArgs;
--- index.js
+++ index.js
@@ -1,57 +1,18 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const getArgs = require("./helpers/args");
 
-const server = http.createServer((req, res) => {
-  if (req.method == "GET") {
-    res.writeHead(200, { "Content-Type": "text/html; charset: utf-8" });
-    if (req.url == "/") {
-      fs.readFile(
-        path.join(__dirname, "templates", "index.html"),
-        "utf-8",
-        (err, content) => {
-          if (err) throw err;
-          res.end(content);
-        }
-      );
-    } else if (req.url == "/about") {
-      fs.readFile(
-        path.join(__dirname, "templates", "about.html"),
-        "utf-8",
-        (err, content) => {
-          if (err) throw err;
-          res.end(content);
-        }
-      );
-    } else if (req.url == "/contact") {
-      fs.readFile(
-        path.join(__dirname, "templates", "contact.html"),
-        "utf-8",
-        (err, content) => {
-          if (err) throw err;
-          res.end(content);
-        }
-      );
-    } else if (req.url == "/api/user") {
-      const user = { name: "Ali", job: "Software engineer" };
-      res.writeHead(200, { "content-type": "text/json" });
-      res.end(JSON.stringify(user));
-    }
-  } else if (req.method == "POST") {
-    const body = [];
-    res.writeHead;
-    req.on("data", (data) => {
-      console.log("da");
-      console.log(data);
-      body.push(data);
-    });
-    req.on("end", () => {
-      console.log(body);
-      res.end("Successfully entred: " + body.toString().split("=")[1]);
-    });
+const startCli = () => {
+  const args = getArgs(process.argv);
+  console.log(args);
+  if(args.h){
+    //help
   }
-});
+  if(args.s){
+    //save
+  }
+  if(args.t){
+    //token
+  }
+  //result
+};
 
-server.listen(4321, () => {
-  // console.log(e);
-});
+startCli();
